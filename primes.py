38c2c6dade5c9aa30e24a809b6abd33d3617ab10
min jung <min.jung@kcl.ac.uk>
2022-09-28 18:28:11 +0100

    add prime number generator and value error exception

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError(f"{number_of_primes} should be greater than 0")
+    list = [2]
+    num = 2
+    while len(list) < number_of_primes:
+        num += 1
+        if is_prime(num):
+            list.append(num)
     return list
+
+def is_prime(num):
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
